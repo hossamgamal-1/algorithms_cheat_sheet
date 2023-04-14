422eafe6da9e1b9e96e33572e84317cad249043a
hossamgamal-1 <hossamgmal1313@gmail.com>
2023-04-15 01:09:15 +0200

    insertion sort

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,8 @@ main() {
   // binarySearch(unsortedList, 54);
   // customNaiveSearch('This is a dummy text', 'dummy');
   // bubleSort(unsortedList);
-  selectionSort(unsortedList);
+  // selectionSort(unsortedList);
+  insertionSort(unsortedList);
 }
 
 int linearSearch(List<int> list, int value) {
@@ -77,6 +78,22 @@ List<int> selectionSort(List<int> list) {
       int temp = list[i];
       list[i] = list[leastValueIndex];
       list[leastValueIndex] = temp;
+    }
+  }
+  return list;
+}
+
+List<int> insertionSort(List<int> list) {
+  for (int i = 0; i < list.length - 1; i++) {
+    for (int j = i + 1; j > 0; j--) {
+      if (list[j - 1] > list[j]) {
+        //swapping
+        int temp = list[j - 1];
+        list[j - 1] = list[j];
+        list[j] = temp;
+      } else {
+        break;
+      }
     }
   }
   return list;

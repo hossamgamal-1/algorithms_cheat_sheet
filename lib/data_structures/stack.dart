d504b97553f5f22b10c4583c62bf25d9765ab7ce
hossamgamal-1 <hossamgmal1313@gmail.com>
2023-04-16 01:43:19 +0200

    stack

---
+++ lib/data_structures/stack.dart
@@ -0,0 +1,19 @@
+// stack using list
+class Stack<T> {
+  List<T> _stack = [];
+  int _top = -1;
+
+  void push(T element) {
+    _stack.add(element);
+    _top++;
+  }
+
+  void pop() {
+    _stack.removeLast();
+    _top--;
+  }
+
+  T? getTop() => _top > -1 ? _stack[_top] : null;
+
+  void display() => print(_stack.reversed);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+import 'data_structures/stack.dart';
 import 'sorting/quick_sort.dart';
 
 main() {
   List<int> unsortedList = [90, 87, 48, 13, 28, 45, 91, 54, 6, 38, 60, 32, 20];
-  // print(linearSearch(unsortedList, 54));
-  // print(binarySearch(unsortedList..sort(), 54));
-  // print(customNaiveSearch('This is a dummy text', 'dummy'));
-  // print(bubleSort(unsortedList));
-  // print(selectionSort(unsortedList));
-  // print(insertionSort(unsortedList));
   print(quickSort(unsortedList));
+  Stack<int> stack = Stack<int>();
+  stack.push(7);
+  stack.push(9);
+  stack.push(12);
+
+  stack.push(1);
+  stack.pop();
+  stack.display();
 }
